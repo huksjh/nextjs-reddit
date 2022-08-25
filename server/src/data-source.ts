e5b5e1a41c9adae1f27b81e5c94c28d16db28f9f
huks <wkdgur1209@gmail.com>
2022-08-26 00:03:01 +0900

    수정

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import dotenv from "dotenv";
 dotenv.config();
+
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: "localhost",
